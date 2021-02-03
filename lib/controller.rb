0195e1aea36226bd1c3e001be99d95f72697dea8
Jaclyn <jaclynnbarrera@gmail.com>
2021-02-03 02:06:53 -0500

    adds prompt method for user input

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,6 +3,12 @@ class Controller
 
     def welcome
         puts "Welcome to Garden Zones!"
+        self.get_zip
+    end
+
+    def get_zip
+        puts "Enter your zipcode to find out what gardening zone you're in."
+        input = gets.strip
     end
     
 end

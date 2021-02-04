9312024725585a71abd47972bb327da233fba479
Jaclyn <jaclynnbarrera@gmail.com>
2021-02-04 15:42:20 -0500

    cleans up user flow in controller classand adds user_options method

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,18 +3,18 @@ class Controller
 
     def welcome
         puts "Welcome to Garden Zones!"
+        puts "Enter your zipcode to find out what gardening zone you're in"
         self.get_zip
     end
 
     def get_zip
-        puts "Enter your zipcode to find out what gardening zone you're in."
         input = gets.strip
         zone = Api.get_zone_by_zip(input).tr('^0-9', '')
-        self.user_prompt(zone)
+        instance = Scraper.get_plants_by_zone(zone)
+        self.user_prompt(instance)
     end
 
-    def user_prompt(zone)
-        instance = Scraper.get_plants_by_zone(zone)
+    def user_prompt(instance)
         puts "You are in gardening zone #{instance.zone}!"
         sleep 1
         puts "Here are 5 plants you can grow in your zone."
@@ -29,13 +29,25 @@ class Controller
     def veggie_info(input,instance)
         results = instance.veggie_data(input)
         puts "Botanical Name: #{results[:botanical_name]}"
+        sleep 1
         puts "Type of plant: #{results[:plant_type]}"
+        sleep 1
         puts "Sun Exposure: #{results[:sun_exposure]}"
-        puts "Would you like to select another plant?"
-        input = gets.strip
+        self.user_options(instance)
     end
 
-    def user_prompt_two
-        puts "Would you like to select another plant?"
+    def user_options(instance)
+        puts "To see information on another plant in your zone, please enter 1"
+        puts "To exit the program, please enter 2" 
+        input = gets.chomp
+        if input == "1"
+            self.user_prompt(instance)
+        elsif input == "2"
+            self.exit_program
+        end 
+    end
+
+    def exit_program
+        abort("Thanks for using the garden zone app!")
     end
 end

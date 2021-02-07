781e35dfbea5462620b7a8b9477ab1ed696a5c84
Jaclyn <jaclynnbarrera@gmail.com>
2021-02-07 16:46:10 -0500

    adds error handling to user_options and plant_selection methods in Controller class

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,7 @@
 
 class Controller
-
-    def welcome
+   
+    def welcome 
         puts "🪴🪴 Welcome to Garden Zones!🪴🪴".green
         sleep 1
         puts "Enter your zipcode to find out what gardening zone you're in."
@@ -35,29 +35,40 @@ class Controller
         puts "Here are 5 plants you can grow in your zone."
         sleep 1
         list = instance.plants.keys.sample(5)
-        list.each.with_index(1) {|item, index| puts "#{index}. #{item}"}
-        puts "Please select the plant you want to learn more about (1-5)."
-        input = list[(gets.strip.to_i - 1)]
-        self.plant_info(input,instance)
+        list.each.with_index(1) {|item, index| puts "#{index}. #{item}".green}
+        puts "Please select (1-5) for the plant you want to learn more about."
+        self.plant_selection(instance,list)
+    end 
+
+    def plant_selection(instance,list)
+        input = gets.strip.to_i
+        if input.between?(1,5)
+            input = list[input-1]
+            self.plant_info(input,instance)
+        else
+            puts "Invalid selection. Please select between 1-5.".red
+            self.plant_selection(instance,list)
+        end
     end
 
     def plant_info(input,instance)
-        emojis = ["🌱", "👩🏻‍🌾", "🌼", "🌾", "🌵", "🍃", "🪴"]
+        emojis = ["🌱", "👩🏻‍🌾", "🌼", "🌾", "🌵", "🍃", "🪴", "🌞", "🌻"]
         results = instance.plant_data(input)
         results.each do |key,value|
             if !results[key].empty?
             puts "#{key.to_s.gsub('_',' ').split.map(&:capitalize).join(' ')}: #{value} #{emojis.sample}".green
-            sleep 1
+            sleep 0.5
             end
         end 
         self.user_options(instance)
     end
 
     def user_options(instance)
-        puts "To see information on another plant in your zone, please enter 1."
+        puts "To see information on another plant in your zone, enter 1."
         puts "To enter different zip code, enter 2."
         puts "To exit the program, please enter 3." 
         input = gets.chomp
+
         if input == "1"
             self.user_prompt(instance)
         elsif input == "2"
@@ -65,6 +76,9 @@ class Controller
             self.get_zip
         elsif input == "3"
             self.exit_program
+        else 
+            puts "Invalid selection.".red
+            self.user_options(instance)
         end 
     end
 
